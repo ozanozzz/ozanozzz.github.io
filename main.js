b5b6091605948a6ef5696d0bcaf7692f88a9575a
ozanozzz <106697783+ozanozzz@users.noreply.github.com>
2024-08-31 14:35:00 +0000

    onlt rect

--- main.js
+++ main.js
@@ -16,41 +16,8 @@ const rectangle = new THREE.Mesh(
   new THREE.PlaneGeometry(50, 50),
   new THREE.MeshBasicMaterial({ color: 0x0000ff }) // blue
 );
-rectangle.position.set(100, 100, 0);
+rectangle.position.set(0, 0, 0); // Set the position to the center of the canvas
 scene.add(rectangle);
-
-// Variables to store the mouse position and whether the rectangle is being dragged
-let isDragging = false;
-let offsetX = 0;
-let offsetY = 0;
-
-// Event listeners for mouse down, up, and move
-document.addEventListener('mousedown', (event) => {
-  if (event.target === renderer.domElement) {
-    const raycaster = new THREE.Raycaster();
-    const mousePosition = new THREE.Vector2();
-    mousePosition.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-    mousePosition.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
-    raycaster.setFromCamera(mousePosition, camera);
-    const intersects = raycaster.intersectObjects([rectangle]);
-    if (intersects.length > 0) {
-      isDragging = true;
-      offsetX = rectangle.position.x - event.clientX;
-      offsetY = rectangle.position.y - event.clientY;
-    }
-  }
-});
-
-document.addEventListener('mouseup', () => {
-  isDragging = false;
-});
-
-document.addEventListener('mousemove', (event) => {
-  if (isDragging) {
-    rectangle.position.x = event.clientX + offsetX;
-    rectangle.position.y = event.clientY + offsetY;
-  }
-});
 
 // Animate the rectangle
 function animate() {
