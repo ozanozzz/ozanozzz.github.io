f85bd6abd5abad0d527eb53dba5f2a2094a22a11
ozanozzz <106697783+ozanozzz@users.noreply.github.com>
2024-08-31 15:06:11 +0000

    no go

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x000000 } ); /* Changed to black */
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
