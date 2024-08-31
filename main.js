5d60ea6fd683b655b4d82c6299be728e1dba6efa
ozanozzz <106697783+ozanozzz@users.noreply.github.com>
2024-08-31 13:13:13 +0000

    50x50

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ let cameraZoom = 1;
 // Rectangle properties
 let rectangleX = 6;
 let rectangleY = 6;
-let rectangleWidth = 200;
-let rectangleHeight = 200;
+let rectangleWidth = 50;
+let rectangleHeight = 50;
 
 // Function to draw the rectangle
 function drawRectangle() {
